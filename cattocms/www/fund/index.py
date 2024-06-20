f56c9ba78052530e1952e0ecb675d0d828311559
phani <saioverhere@gmail.com>
2024-06-20 23:27:20 +0530

    changes

--- cattocms/www/fund/index.py
+++ cattocms/www/fund/index.py
@@ -5,6 +5,6 @@ def get_context(context):
 	context.caselist = frappe.get_all(
 		"Case",
         {"enable_funding":1},
-		["name", "main_title", "case_image", "enable_funding", "target_amount"],
+		["name", "main_title", "case_image", "enable_funding", "target_amount", "sathi_name", "raising_for"],
 		order_by="idx"
 	)
