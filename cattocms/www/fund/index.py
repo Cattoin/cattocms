cc766ad6a32884d769b923ddaaef75108873eac5
ph4ni <29685411+ph4ni@users.noreply.github.com>
2024-08-20 23:45:19 +0530

    changed forms

--- cattocms/www/fund/index.py
+++ cattocms/www/fund/index.py
@@ -4,7 +4,7 @@ def get_context(context):
 	context.no_cache = 1
 	context.caselist = frappe.get_all(
 		"Case",
-        {"enable_funding":1},
-		["name", "main_title", "case_image", "enable_funding", "target_amount", "sathi_name", "raising_for"],
+        {"fundraising_status":"Ongoing"},
+		["name", "main_title", "case_image", "fundraising_status", "target_amount", "sathi_name", "raising_for"],
 		order_by="idx"
 	)
