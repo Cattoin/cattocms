5e05331a9a5ceb3565b224a9d273c05ecfcebacf
ph4ni <29685411+ph4ni@users.noreply.github.com>
2024-09-29 18:17:55 +0530

    fixed mobile view

--- cattocms/www/fund/index.py
+++ cattocms/www/fund/index.py
@@ -2,9 +2,16 @@ import frappe
 
 def get_context(context):
 	context.no_cache = 1
-	context.caselist = frappe.get_all(
+	context.ongoing_caselist = frappe.get_all(
 		"Case",
-        {"fundraising_status":"Ongoing"},
+		{"fundraising_status":"Ongoing"},
+		["name", "main_title", "case_image", "fundraising_status", "target_amount", "sathi_name", "raising_for"],
+		order_by="idx"
+	)
+
+	context.completed_caselist = frappe.get_all(
+		"Case",
+		{"fundraising_status":"Completed"},
 		["name", "main_title", "case_image", "fundraising_status", "target_amount", "sathi_name", "raising_for"],
 		order_by="idx"
 	)
