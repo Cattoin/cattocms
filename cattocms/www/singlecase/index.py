12cb6fefa9e93cf404c4f595dd3dda739058f28f
ph4ni <29685411+ph4ni@users.noreply.github.com>
2024-10-16 23:19:27 +0530

    changed donation flow

--- cattocms/www/singlecase/index.py
+++ cattocms/www/singlecase/index.py
@@ -9,7 +9,7 @@ def get_context(context):
     context.case_details = frappe.db.get_value(
         "Case",
 	    caseid,
-        ["name","main_title", "beneficiary_name", "case_image", "sathi_name", "target_amount", "case_details", "sathi_details"],
+        ["name","main_title", "beneficiary_name", "case_image", "sathi_name", "target_amount", "case_details", "sathi_details", "raised_amount"],
         as_dict=1,
 	)
     context.site_name = get_site_name(frappe.local.request.host)
